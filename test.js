87fbcfd1610ed098701d4ff0106885ba28e2e227
Parsha Pourkhomami <parshap+git@gmail.com>
2014-09-02 11:53:26 -0700

    Add failing test for stream output

--- test.js
+++ test.js
@@ -8,14 +8,23 @@ test("one at a time", function(t) {
 		waiting = true;
 		setTimeout(function() {
 			waiting = false;
-			callback();
+			callback(null, data);
 		}, 100);
 	}, 1);
+	stream.write("a");
+	stream.write("b");
+	stream.write("c");
+	stream.end();
+
+	var datas = [];
+	stream.on("data", function(data) {
+		datas.push(data);
+	});
 	stream.on("end", function() {
+		t.equal(datas.length, 3);
+		t.equal(datas[0], "a");
+		t.equal(datas[1], "b");
+		t.equal(datas[2], "c");
 		t.end();
 	});
-	stream.write("a");
-	stream.write("a");
-	stream.write("a");
-	stream.end();
 });
